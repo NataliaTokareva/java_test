7d5144af9688ab34745de095872968f1725ad5bc
NataliaTokareva <n.tokareva@quality-lab.ru>
2020-05-31 22:43:05 +0300

    выполнено ДЗ №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -1,28 +1,50 @@
 package ru.stqa.pft.sandbox;
 
+import javax.sql.XAConnection;
+
 public class myFirstProgram {
 
-    public static void main(String[] args) {
-        hello("world");
-        hello("user");
-        hello("Natasha");
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Natasha");
 
-        double l = 5;
-      System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
 
-      double a = 4;
-      double b = 6;
-      System.out.println("Площадь квадрата со сторонами " + a + " и " + b + " = " + area(a, b));
 
-    }
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    public static void hello(String somebody)  {
-      System.out.println("Hello, " + somebody + "!");
-    }
-public static double area(double len) {
-   return len*len;
-}
-public static double area(double a,double b) {
-      return a * b;
-}
+    Rectangle r = new Rectangle(4, 6);
+
+
+    System.out.println("Площадь квадрата со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+
+//функция вывода с атрибутами которая использует функцию вычисления расстояния
+    Point p = new Point(20, 16);
+  //  p.p1 = 20;
+   // p.p2 = 16;
+
+
+  //  System.out.println("Расстояние между двумя точками на плоскости c координатами " + p.p1 + " и " + p.p2 +  " = " + distance(p));
+
+    //метод
+    System.out.println("Расстояние между двумя точками на плоскости c координатами " + p.p1 + " и " + p.p2 +  " = " + p.distance());
+
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(Rectangle r) {
+    return r.a * r.b;
+  }
+//функция вычисления расстояния
+ // public static double distance(Point p) {
+  //  return Math.sqrt(p.p1 + p.p2);
+
+  //}
+
 }
