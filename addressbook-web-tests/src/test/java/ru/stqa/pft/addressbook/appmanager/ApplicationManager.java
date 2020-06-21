f527b71305ca39f5b599c63f1f2ed2f6b20188d5
NataliaTokareva <n.tokareva@quality-lab.ru>
2020-06-21 18:02:24 +0300

    в ApplicationManager добавлены классы помощники

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,67 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+  protected WebDriver driver;
+  private SessionHelper sessionHelper;
+  private  NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+
+  protected StringBuffer verificationErrors = new StringBuffer();
+  private boolean acceptNextAlert = true;
+
+  public void init() {
+    driver = new FirefoxDriver();
+    String baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.get("http://localhost/addressbook/");
+    groupHelper = new GroupHelper(driver);
+    navigationHelper = new NavigationHelper(driver);
+    sessionHelper = new SessionHelper(driver);
+    sessionHelper.login("admin", "secret");
+  }
+
+
+
+  public void stop() {
+    driver.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
+}
