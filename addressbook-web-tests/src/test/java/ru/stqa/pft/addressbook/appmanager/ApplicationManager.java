743ef222f9b34d68c06e6234f053391945bca71f
NataliaTokareva <n.tokareva@quality-lab.ru>
2020-07-01 13:47:38 +0300

    внесение конструктора по выбору браузера при выполнении тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,9 +16,20 @@ public class ApplicationManager {
 
   public StringBuffer verificationErrors = new StringBuffer();
   private boolean acceptNextAlert = true;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    driver = new FirefoxDriver();
+    if (browser == BrowserType.FIREFOX) {
+      driver = new FirefoxDriver();
+    } else if (browser == BrowserType.CHROME) {
+      driver = new ChromeDriver();
+    } else if (browser == BrowserType.EDGE) {
+      driver = new InternetExplorerDriver();
+  }
     String baseUrl = "https://www.google.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     driver.get("http://localhost/addressbook/");
